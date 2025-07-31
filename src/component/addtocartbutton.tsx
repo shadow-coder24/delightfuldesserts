875e0eb6bcb9e1b5acb16ccb037b13058708be71
shadow-coder24 <shadowcoder24@gmail.com>
2025-07-31 19:42:25 +0530

    a

--- src/component/addtocartbutton.tsx
+++ src/component/addtocartbutton.tsx
@@ -2,8 +2,20 @@
 import React, { memo, useContext } from "react";
 import cartContext from "@/context/cartcontext";
 
+type Product = {
+  productid: number;
+  productname: string;
+  productdescription: string;
+  productprice: number;
+  productimage: string;
+};
+
+type Props = {
+  product: Product;
+};
+
 // eslint-disable-next-line react/display-name
-export const Addtocartbutton = memo(({ product }) => {
+export const Addtocartbutton = memo(({ product }: Props) => {
   const { cart, addtocart, removefromcart } = useContext(cartContext);
 
   const increase = () => {
